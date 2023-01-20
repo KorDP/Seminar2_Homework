682d454979c507a2bcef8dfc7144da9b338c8874
KorDP <nukedmitry@gmail.com>
2023-01-20 18:56:59 +0300

    task 10 done

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите 3 значное число: ");
+int ShowMidNum(int number)
+{
+    int remain = number % 100;
+    return remain / 10;
+}
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{
+Console.WriteLine("Вы ввели не 3 значное число");
+}
+else
+{
+Console.WriteLine(ShowMidNum(num));
+}
