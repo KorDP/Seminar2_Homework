6432946f583a90480bd25a7654a2caa08ecee5fe
KorDP <nukedmitry@gmail.com>
2023-01-20 19:49:57 +0300

    task 15 done

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число от обозначающее день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day <= 7)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Выходной день");
+    } 
+    else
+    {
+        Console.WriteLine("Будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
