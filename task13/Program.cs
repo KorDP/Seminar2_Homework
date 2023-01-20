6f11bb8d49c4a9bd08373e2fe7c586a98130e5ba
KorDP <nukedmitry@gmail.com>
2023-01-20 19:45:22 +0300

    task 13 done

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿int ThirdDigit(int number)
+ {
+     int result = -1;
+     if (number >= 100)
+     {
+        while (number > 999)
+        {
+            number = number / 10;
+            }
+            result = number % 10;
+            }
+            return result;
+            }
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (ThirdDigit(number) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+{
+Console.WriteLine ($"{ThirdDigit (number)}");
+}
